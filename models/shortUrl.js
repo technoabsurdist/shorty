cf5224dfbf16da6741c14af836ad7efdc3c5cc25
Emi <92827957+emiandere@users.noreply.github.com>
2022-06-12 11:08:57 -0500

    whole tortilla

---
+++ models/shortUrl.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+const shortId = require('shortid')
+
+
+const shortUrlSchema = mongoose.Schema({
+    full: {
+        type: String, 
+        required: true, 
+    }, 
+    short: {
+        type: String, 
+        required: true, 
+        default: shortId.generate
+    }
+})
+
+module.exports = mongoose.model("ShortUrl", shortUrlSchema)
+
